97bad23c67f54d41203df8cb45c293823d622027
Ignacio Machuca <ignacio.machuca@ug.uchile.cl>
2017-05-24 18:23:47 -0400

    Adapted signup template and implemented client creation.

--- tarea3/app/views.py
+++ tarea3/app/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from .models import Alumno
 
 
 def index(request):
@@ -11,4 +13,27 @@ def login(request):
 
 
 def signup(request):
-    return HttpResponse(render(request, 'app/signup.html', {}))
+    if request.method == 'POST':
+        # tipo de usuario
+        tipo = request.POST['tipo']
+        # nombre de la persona
+        nombre = request.POST['nombre']
+        # correo usuario, usado también como username
+        email = request.POST['email']
+
+        foto_perfil = request.POST['foto_perfil']
+        contraseña = request.POST['password']
+        contraseña_r = request.POST['password2']
+
+        if contraseña == contraseña_r:
+            if tipo == '3':
+                if contraseña == contraseña_r:
+                    u = User.objects.create_user(email, email, contraseña)
+                    u.first_name = nombre
+                    u.save()
+                    Alumno.objects.create(user=u)
+                    return HttpResponse("Usuario creado")
+            return HttpResponse("Usuario no creado")
+
+    else:
+        return HttpResponse(render(request, 'app/signup.html', {}))
