7c349cc644c73bae333fb62f1b1ed14dccb9c577
Ignacio Machuca <liberty.fik@gmail.com>
2017-05-28 18:45:54 -0400

    Implemented user login.

--- tarea3/app/urls.py
+++ tarea3/app/urls.py
@@ -7,5 +7,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login/$', views.login, name='login'),
-    url(r'^signup/$', views.signup, name='signup')
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^vendedor/$', views.vendedor, name='vendedor'),
 ]
--- tarea3/app/views.py
+++ tarea3/app/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as enter
 from django.contrib.auth.models import User, Group
 from .models import Alumno
 
@@ -9,7 +10,19 @@ def index(request):
 
 
 def login(request):
-    return HttpResponse(render(request, 'app/login.html', {}))
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            enter(request, user)
+            return redirect('index')
+        else:
+            #TODO return error, 403 or w/e
+            return redirect('login')
+
+    else:
+        return HttpResponse(render(request, 'app/login.html', {}))
 
 
 def signup(request):
@@ -27,13 +40,19 @@ def signup(request):
 
         if contraseña == contraseña_r:
             if tipo == '3':
-                if contraseña == contraseña_r:
-                    u = User.objects.create_user(email, email, contraseña)
-                    u.first_name = nombre
-                    u.save()
-                    Alumno.objects.create(user=u)
-                    return HttpResponse("Usuario creado")
+                u = User.objects.create_user(email, email, contraseña)
+                u.first_name = nombre
+                u.save()
+                Alumno.objects.create(user=u)
+                return HttpResponse("Usuario creado")
             return HttpResponse("Usuario no creado")
+
+        else:
+            return HttpResponse(render(request, 'app/signup.html', {'errores': "las constraseñas no coinciden"}))
 
     else:
         return HttpResponse(render(request, 'app/signup.html', {}))
+
+
+def vendedor(request):
+    return HttpResponse(render(request, 'app/vendedor-profile-page.html', {}))
